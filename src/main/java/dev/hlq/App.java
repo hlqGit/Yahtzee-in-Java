273046c22c1cec660f39522a1e9dfc4cb29f8546
hlq <justinjoslin0@gmail.com>
2024-02-20 00:47:55 -0600

    final fix

--- src/main/java/dev/hlq/App.java
+++ src/main/java/dev/hlq/App.java
@@ -13,7 +13,7 @@ public class App extends Application{
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 800, 1200);
+        scene = new Scene(loadFXML("Main"), 800, 1200);
         stage.setScene(scene);
         stage.show();
     }
