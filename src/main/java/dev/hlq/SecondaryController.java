f50308dee6836ad0946433c48765104af5866f60
hlq <justinjoslin0@gmail.com>
2024-02-21 12:56:00 -0600

    close resolution tab after use

--- src/main/java/dev/hlq/SecondaryController.java
+++ src/main/java/dev/hlq/SecondaryController.java
@@ -3,30 +3,35 @@ package dev.hlq;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.input.InputEvent;
 import javafx.stage.Stage;
 
 public class SecondaryController {
-    @FXML
-    private Button resolution1;
-    @FXML
-    private Button resolution2;
 
     private static Scene scene;
 
-    public void resolution1Pressed() throws IOException{
+    @FXML public void resolution1Pressed(InputEvent e) throws IOException{
         Stage mainStage = new Stage();
         scene = new Scene(App.loadFXML("Main"));
         mainStage.setScene(scene);
         mainStage.show();
         mainStage.setResizable(false);
+
+        final Node source = (Node) e.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
     }
-    public void resolution2Pressed() throws IOException{
+    @FXML public void resolution2Pressed(InputEvent e) throws IOException{
         Stage mainStage = new Stage();
         scene = new Scene(App.loadFXML("Larger"));
         mainStage.setScene(scene);
         mainStage.show();
         mainStage.setResizable(false);
+
+        final Node source = (Node) e.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
     }
 }
