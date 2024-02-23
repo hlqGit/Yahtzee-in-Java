e5ab954b279e25c90c6cf4f368c4bc92b8bfd084
hlq <justinjoslin0@gmail.com>
2024-02-23 10:47:34 -0600

    removed unused import

--- src/main/java/dev/hlq/Controller.java
+++ src/main/java/dev/hlq/Controller.java
@@ -5,7 +5,6 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.InputEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
